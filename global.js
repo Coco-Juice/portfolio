22f1d07cd95eeb5a38d9be806f64185d9604897c
Coco-Juice <tonybai321@gmail.com>
2025-10-13 17:01:13 -0700

    implement automatic nav menu

--- global.js
+++ global.js
@@ -4,9 +4,39 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-let navLinks = $$("nav a");
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'contacts/', title: 'Contacts' },
+  { url: 'resume.html', title: 'Resume' },
+  { url: 'https://github.com/Coco-Juice', title: 'GitHub' }
+];
 
-let currentLink = navLinks.find(
-  (a) => a.host === location.host && a.pathname === location.pathname,
-);
-currentLink?.classList.add('current');
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  // Local server
+  : "/website/";         // GitHub Pages repo name
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+
+    url = !url.startsWith('http') ? BASE_PATH + url : url;
+
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    nav.append(a);
+    
+    if (a.host === location.host && a.pathname === location.pathname) {
+        a.classList.add('current');
+    }
+    if (a.host !== location.host) {
+        a.target = "_blank";
+    }
+}
+
+
+
