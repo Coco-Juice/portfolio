4e651bf2149639fd30fda056c3e0751e6b818634
Coco-Juice <tonybai321@gmail.com>
2025-10-21 12:15:26 -0700

    added project counter to title

--- projects/projects.js
+++ projects/projects.js
@@ -2,9 +2,13 @@ import { fetchJSON, renderProjects } from '../global.js';
 
 const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
+const projectsTitle = document.querySelector('.projects-title');
+
 projectsContainer.innerHTML = '';
 if (Array.isArray(projects)) {
   projects.forEach(project => renderProjects(project, projectsContainer));
+  projectsTitle.textContent = projects.length;
 } else {
   console.error('Invalid projects data:', projects);
+  projectsTitle.textContent = '0';
 }
