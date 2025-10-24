b66cd8cc5318696f336a410191ad6716bfcf896e
Coco-Juice <tonybai321@gmail.com>
2025-10-23 20:20:00 -0700

    reimplemented project count

--- projects/projects.js
+++ projects/projects.js
@@ -5,3 +5,4 @@ const projectsContainer = document.querySelector('.projects');
 const projectsTitle = document.querySelector('.projects-title');
 
 renderProjects(projects, projectsContainer, 'h2');
+projectsTitle.textContent = projects.length;
