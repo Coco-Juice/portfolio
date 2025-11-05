2df22bfd9c2cec3420ceb418bb2bfa53bef0ee29
Coco-Juice <tonybai321@gmail.com>
2025-11-05 14:24:57 -0800

    added section for summary stats

--- meta/main.js
+++ meta/main.js
@@ -47,6 +47,40 @@ function processCommits(data) {
     });
 }
 
+function renderCommitInfo(data, commits) {
+    // Create the dl element
+    const dl = d3.select('#stats').append('dl').attr('class', 'stats');
+
+    // Add total LOC
+    dl.append('dt').html('Total <abbr title="Lines of code">LOC</abbr>');
+    dl.append('dd').text(data.length);
+
+    // Add total commits
+    dl.append('dt').text('Commits');
+    dl.append('dd').text(commits.length);
+
+    // Add number of files
+    dl.append('dt').text('Files');
+    dl.append('dd').text(d3.groups(data, d => d.file).length);
+
+    // Add average file length
+    dl.append('dt').text('Avg File Length');
+    dl.append('dd').text(Math.round(averageFileLength));
+
+    // Add longest file
+    dl.append('dt').text('Longest File');
+    dl.append('dd').text(longestFile);
+}
+
 let data = await loadData();
 let commits = processCommits(data);
-console.log(commits);
+
+const fileLengths = d3.rollups(
+  data,
+  (v) => d3.max(v, (v) => v.line),
+  (d) => d.file,
+);
+const averageFileLength = d3.mean(fileLengths, (d) => d[1]);
+const longestFile = d3.greatest(fileLengths, (d) => d[1])?.[0];
+
+renderCommitInfo(data, commits);
