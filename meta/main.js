75591cd4107ce5b7db3d53dd8367e129b8fa5606
Coco-Juice <tonybai321@gmail.com>
2025-11-20 11:15:38 -0800

    implement scrollytelling

--- meta/main.js
+++ meta/main.js
@@ -1,4 +1,5 @@
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+import scrollama from 'https://cdn.jsdelivr.net/npm/scrollama@3.2.0/+esm';
 
 function onTimeSliderChange() {
   const slider = document.getElementById("commit-progress");
@@ -422,3 +423,41 @@ let filteredCommits = commits;
 document.getElementById("commit-progress").addEventListener("input", onTimeSliderChange);
 onTimeSliderChange();
 
+d3.select('#scatter-story')
+  .selectAll('.step')
+  .data(d3.sort(commits, (d) => d.datetime))
+  .join('div')
+  .attr('class', 'step')
+  .html(
+    (d, i) => `
+		On ${d.datetime.toLocaleString('en', {
+      dateStyle: 'full',
+      timeStyle: 'short',
+    })},
+		I made <a href="${d.url}" target="_blank">${
+      i > 0 ? 'another commit' : 'my first commit'
+    }</a>.
+		I edited ${d.totalLines} lines across ${
+      d3.rollups(
+        d.lines,
+        (D) => D.length,
+        (d) => d.file,
+      ).length
+    } files.
+	`,
+  );
+
+function onStepEnter(response) {
+  const commit = response.element.__data__;
+  const t = commit.datetime;
+  const filteredCommits = commits.filter(d => d.datetime <= t);
+  updateScatterPlot(data, filteredCommits);
+}
+
+const scroller = scrollama();
+scroller
+  .setup({
+    container: '#scrolly-1',
+    step: '#scrolly-1 .step',
+  })
+  .onStepEnter(onStepEnter);
